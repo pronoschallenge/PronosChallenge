59eb09331092d5caf08cdb3494f627023077be8c
thomasdelhomenie <thomas.delhomenie@gmail.com>
2011-11-10 23:54:25 +0100

    Ajout de l'action post pour la strategie de l'utilisateur

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <link rel="alternate" type="application/rss+xml" title="PronosChallenge - Gazouillis" href="http://<?php echo $_SERVER['SERVER_NAME']?>/suopronos/prono/rss.php?gazouillis" />
 </head>
 <frameset rows="100%,*" frameborder="no" border="0">
- <frame name="redirection" src="http://www.pronoschallenge.fr/suopronos/prono/">
+ <frame name="redirection" src="http://localhost/PronosChallenge/suopronos/prono/">
  <frame name="vide" src="" scrolling="no" noresize>
 </frameset>
 </body>
