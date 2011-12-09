a1e39fc1d96bf1e104ac9bc40f7cb854e89b5224
smetivie <metivierstephane@hotmail.com>
2011-12-09 20:31:28 +0100

    Liste des amis : service web de liste et d'ajout/suppression des amis

--- suopronos/rest/listeAmis.php
+++ suopronos/rest/listeAmis.php
@@ -7,60 +7,72 @@
  *    - "1" = liste des amis
  *    - "0" = liste des utilisateurs non amis (pour les ajouter)
  * 
- * Méthode POST : doit avoir "action" en paramètre.
+ * Méthode POST : 
+ *  => doit avoir "action" en paramètre.
  *    - "add" = ajout d'un ami
  *    - "del" = suppression d'un ami
+ *  => doit avoir "ami" en paramètre (pseudo de l'ami à ajouter/retirer)
  * 
  * An example resource
  * @uri /listeAmis/(.*)?
  */
 class ListeAmisResource extends Resource {
     
-    function post($request) {
+    function post($request, $user) {
     
     	ouverture ();
     
     	$response = new Response($request);
-    
-    	if(!($this->isSecured())) {
-	    	$response->body = "401";
-	    	return $response;
-    	}
-    
-    	// ami à ajouter/retirer
-    	$ami_pseudo = file_get_contents('php://input');
-    	if(empty($ami_pseudo)) {
+
+    	// récupération des valeurs JSON
+    	/*$json = file_get_contents('php://input');
+    	if(empty($json)) {
     		$response->body = "500:NO_DATA";
-		    return $response;
-    	}
-    	$requete= "SELECT id_prono FROM phpl_membres WHERE pseudo='$ami_pseudo'";
-    	$result = mysql_query($requete);
-    	$row = mysql_fetch_array($result);
-    	$ami_id=$row[0];
-    
-    	// utilisateur postant la requête
-    	$user_pseudo = $_SERVER['PHP_AUTH_USER'];
-    	$requete= "SELECT id_prono FROM phpl_membres WHERE pseudo='$user_pseudo'";
-        $result = mysql_query($requete);
-        $row = mysql_fetch_array($result);
-    	$user_id=$row[0];
-    
+    		return $response;
+    	}    	
+    	$ami = json_decode($json);*/
+    	
+    	
     	// récupération de l'action
     	if(isset($_GET['action']))  {
-    		$action = $_GET['action'];
+    	    $action = $_GET['action'];
     	} else {
-	    	$response->body = "401";
+	    	$response->body = "401\action";
 	    	return $response;
     	}    	 
+    	 
+    	// récupération de l'action
+    	if(isset($_GET['ami']))  {
+    		$ami_pseudo = $_GET['ami'];
+    	} else {
+	    	$response->body = "401\ami";
+	    	return $response;
+    	}   	
+    	
+    	// Récupération des informations de l'ami posté
+    	//$ami_pseudo = $ami->ami;   	
+    	$requete = "SELECT id_prono FROM phpl_membres WHERE pseudo = '$ami_pseudo'";
+    	$result = mysql_query($requete);
+    	$row = mysql_fetch_array($result);
+    	$ami_id = $row[0];
+    
+    	// utilisateur postant la requête
+    	$requete = "SELECT id_prono FROM phpl_membres WHERE pseudo = '$user'";
+        $result = mysql_query($requete);
+        $row = mysql_fetch_array($result);
+    	$user_id = $row[0];
     	
     	if ($action == "add") {
-    		$requeteInsert = "INSERT INTO phpl_clmnt_filtre (id, idMembre, visible)
-    		    			  VALUES ('$user_id', '$ami_id', '1')
-    		    			  WHERE NOT EXISTS (SELECT id FROM phpl_clmnt_filtre WHERE id = '$user_id' AND idMembre = '$ami_id')";
-    		mysql_query($requeteInsert) or die ("probleme " .mysql_error());
+    		$requete = "SELECT id FROM phpl_clmnt_filtre WHERE id = '$user_id' AND idMembre = '$ami_id'";
+    		$result = mysql_query($requete);
+    		if (! mysql_fetch_array($result)) {
+	    		$requeteInsert = "INSERT INTO phpl_clmnt_filtre (id, idMembre, visible)
+	    		    			  VALUES ('$user_id', '$ami_id', '1')";
+	    		mysql_query($requeteInsert) or die ("probleme " .mysql_error());
+    		}
     	} elseif ($action == "del") {
     		$requeteDelete = "DELETE FROM phpl_clmnt_filtre 
-    		    		      WHERE id = '$user_id' AND idMembre = '$ami_id')";
+    		    		      WHERE id = '$user_id' AND idMembre = '$ami_id'";
     		mysql_query($requeteDelete) or die ("probleme " .mysql_error());
     	}
             
