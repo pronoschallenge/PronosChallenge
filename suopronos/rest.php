844dd71ae2d42d997b899787f957b80db00a4d14
smetivie <metivierstephane@hotmail.com>
2012-02-19 23:00:10 +0100

    Ajout du service "calendrierClub" pour retourner le calendrier d'un club

--- suopronos/rest.php
+++ suopronos/rest.php
@@ -23,7 +23,7 @@ require_once './rest/coteMatch.php';
 require_once './rest/confrontationClub.php';
 require_once './rest/gazouillisNonLus.php';
 require_once './rest/evolutionClassementL1.php';
-
+require_once './rest/calendrierClub.php';
 
 $uri = $_SERVER['REQUEST_URI'];
 $config = array();
